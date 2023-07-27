742615ff1c89737c105745b7e1cdb30c9b12af43
Muhammad Shakir <90838750+prodevXpert@users.noreply.github.com>
2023-07-27 20:58:13 +0500

    Add project files.

---
+++ OOPSCourse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace OOPSCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccMod _obj=new AccMod();
+            _obj.PrintData();
+        }
+    }
+
+    class AccMod
+    {
+        public  void PrintData()
+        {
+            Console.WriteLine("I am Data here");
+            Console.ReadLine();
+        }
+    }
+}
